9ab6d3d5998f17486b3ba2b4ea516de3e4617801
bri3d <brian@brianledbetter.com>
2021-01-19 12:20:58 -0700

    add file logger and some convenience output to flasher

--- flashsimos18.py
+++ flashsimos18.py
@@ -1,7 +1,9 @@
 import argparse
+import logging
 import time
 import udsoncan
 from os import path
+from typing import List, Union
 from tqdm import tqdm
 from udsoncan.connections import IsoTPSocketConnection
 from udsoncan.client import Client
@@ -10,9 +12,60 @@ from udsoncan import exceptions
 from udsoncan import services
 from sa2_seed_key.sa2_seed_key import Sa2SeedKey
 
-parser = argparse.ArgumentParser(description='Flash Simos18 ECU.', epilog="For example, --block 1 --file cboot.compressed.encrypted --block 2 asw1.compressed.encrypted")
-parser.add_argument('--block', type=int, action="append",
-                    help='which blocks to flash, numerically. blocks numbered greater than 5 will be flashed as patches to (blocknum - 5)',
+block_name_to_int = {
+  'CBOOT': 1,
+  'ASW1' : 2,
+  'ASW2' : 3,
+  'ASW3' : 4,
+  'CAL' : 5,
+  'PATCH_ASW1': 7,
+  'PATCH_ASW2': 8,
+  'PATCH_ASW3': 9
+}
+
+int_to_block_name = dict((reversed(item) for item in block_name_to_int.items()))
+
+block_lengths = {
+  1: 0x23e00, # CBOOT
+  2: 0xffc00, # ASW1
+  3: 0xbfc00, # ASW2
+  4: 0x7fc00, # ASW3
+  5: 0x7fc00 # CAL
+}
+
+# We can send the maximum allowable size worth of compressed data in an ISO-TP request when we are using the "normal" TransferData system.
+block_transfer_sizes = {
+  1: 0xFFD,
+  2: 0xFFD,
+  3: 0xFFD,
+  4: 0xFFD,
+  5: 0xFFD
+}
+
+# When we're performing WriteWithoutErase, we need to write 8 bytes at a time in "patch areas" to allow the ECC operation to be performed correctly across the patched data.
+# But, when we're just "writing" 0s (which we can't actually do), we can go faster and fill an entire 256-byte Assembly Page in the flash controller as ECC will not work anyway.
+# Internally, we're basically stuffing the Assembly Page for the flash controller and the method return does not wait for controller readiness, so we will also need to resend data repeatedly.
+def block_transfer_sizes_patch(block_number: int, address: int) -> int:
+  if(block_number != 4):
+    print("Only patching H__0001's Block 4 / ASW3 using a provided patch is supported at this time! If you have a patch for another block, please fill in its data areas here.")
+    exit()
+  if(address < 0x95FF):
+    return 0x100
+  if(address >= 0x95FF and address < 0x9800):
+    return 0x8
+  if(address >= 0x9800 and address < 0x7DD00):
+    return 0x100
+  if(address >= 0x7DD00 and address < 0x7E200):
+    return 0x8
+  if(address >= 0x7E200):
+    return 0x100
+  return 0x8
+
+block_names_help = "\n".join([' = '.join([name, str(number)]) for (name, number) in block_name_to_int.items()])
+
+parser = argparse.ArgumentParser(description='Flash Simos18 ECU.', epilog="For example, --block CBOOT --file cboot.compressed.encrypted --block ASW1 asw1.compressed.encrypted")
+parser.add_argument('--block', type=str, action="append",
+                    help='which blocks to flash, numerically or by name. ' + block_names_help,
                     required=True)
 parser.add_argument('--file', type=str, action="append",
                     help='which blocks to flash, numerically',
@@ -22,8 +75,15 @@ parser.add_argument('--tunertag', type=str, default="",
 
 args = parser.parse_args()
 
+def block_to_number(blockname: str) -> int:
+  if blockname.isdigit():
+    return blockname
+  else:
+    return block_name_to_int[blockname]
+
+int_blocks = [block_to_number(block) for block in args.block]
 # We rely on dict retaining insertion order which is part of the language as of 3.7
-block_files = dict(zip(args.block, args.file))
+block_files = dict(zip(int_blocks, args.file))
 
 tuner_tag = args.tunertag
 
@@ -56,7 +116,7 @@ class GenericBytesCodec(udsoncan.DidCodec):
   def __len__(self):
     raise udsoncan.DidCodec.ReadAllRemainingData
 
-data_records = [
+data_records : List[DataRecord] = [
   DataRecord(0xF190, 0, "VIN Vehicle Identification Number"),
   DataRecord(0xF19E, 0, "ASAM/ODX File Identifier"),
   DataRecord(0xF1A2, 0, "ASAM/ODX File Version"),
@@ -109,45 +169,15 @@ data_records = [
   DataRecord(0xF17E,0,"ECU Production Change Number")
 ]
 
-def volkswagen_security_algo(level, seed, params=None):
+def volkswagen_security_algo(level: int, seed: bytes, params=None) -> bytes:
   simos18_sa2_script = bytearray([0x68, 0x02, 0x81, 0x4A, 0x10, 0x68, 0x04, 0x93, 0x08, 0x08, 0x20, 0x09, 0x4A, 0x05, 0x87, 0x22, 0x12, 0x19, 0x54, 0x82, 0x49, 0x93, 0x07, 0x12, 0x20, 0x11, 0x82, 0x4A, 0x05, 0x87, 0x03, 0x11, 0x20, 0x10, 0x82, 0x4A, 0x01, 0x81, 0x49, 0x4C])
   vs = Sa2SeedKey(simos18_sa2_script, int.from_bytes(seed, "big"))
   return vs.execute().to_bytes(4, 'big')
 
-block_lengths = {
-  1: 0x23e00,
-  2: 0xffc00,
-  3: 0xbfc00,
-  4: 0x7fc00,
-  5: 0x7fc00
-}
-block_transfer_sizes = {
-  1: 0xFFD,
-  2: 0xFFD,
-  3: 0xFFD,
-  4: 0xFFD,
-  5: 0xFFD
-}
-
-# When we're performing WriteWithoutErase, we need to write very slowly to allow the un-erased flash to soak - but when we're just "writing" 0s (which we can't actually do), we can go faster.
-# Internally, we're basically stuffing the mailbox for a flash task, so we also need to repeat many requests as the mailbox will be full but the prior transfer request does not wait for readiness to return. 
-def block_transfer_sizes_patch(block_number, address):
-  if(block_number != 4):
-    print("Only patching Block 4 / ASW3 is supported at this time!")
-    exit()
-  if(address < 0x95FF):
-    return 0x100
-  if(address >= 0x95FF and address < 0x9800):
-    return 0x8
-  if(address >= 0x9800 and address < 0x7DD00):
-    return 0x100
-  if(address >= 0x7DD00 and address < 0x7E200):
-    return 0x8
-  if(address >= 0x7E200):
-    return 0x100
-  return 0x8
-
 udsoncan.setup_logging(path.join(path.dirname(path.abspath(__file__)), 'logging.conf'))
+logger = logging.getLogger("VWFlash")
+logger.info("Started with configuration: " + str(block_files))
+print("Preparing to flash the following blocks: " + str(block_files))
 
 params = {
   'tx_padding': 0x55
@@ -183,13 +213,19 @@ with Client(conn, request_timeout=5, config=configs.default_client_config) as cl
       print("Opening extended diagnostic session...")
       client.change_session(services.DiagnosticSessionControl.Session.extendedDiagnosticSession)
 
+      vin_did = data_records[0]
+      vin: str = client.read_data_by_identifier_first(vin_did.address)
+      print("Extended diagnostic session connected to vehicle with VIN: " + vin)
+      logger.info(vin + " Connected: Flashing blocks: " + str(block_files))
+
       print("Reading ECU information...")
       for i in range(33, 47):
         did = data_records[i]
         response = client.read_data_by_identifier_first(did.address)
         print(did.description + " : " + response)
+        logger.info(vin + " " + did.description + " : " + response)
 
-      # Check Programming Precondition (load CBOOT)
+      # Check Programming Precondition
       print("Checking programming precondition, routine 0x0203...")
       client.start_routine(0x0203)
 
@@ -205,19 +241,19 @@ with Client(conn, request_timeout=5, config=configs.default_client_config) as cl
 
       client.tester_present()
 
-      # Perform Seed/Key Security Level 17
+      # Perform Seed/Key Security Level 17. This will call volkswagen_security_algo above to perform the Seed/Key auth against the SA2 script.
       print("Performing Seed/Key authentication...")
       client.unlock_security_access(17)
 
       client.tester_present()
 
       print("Writing flash tool log to LocalIdentifier 0xF15A...")
-      # Write Flash Tool Workshop Log (TODO real/fake date/time)
+      # Write Flash Tool Workshop Log (TODO real/fake date/time, currently hardcoded to 2014/7/17)
       client.write_data_by_identifier(0xF15A, bytes([
-          0x14,
-          0x7,
-          0x17,
-          0x0,
+          0x14, # Year (BCD/HexDecimal since 2000)
+          0x7, # Month (BCD)
+          0x17, # Day (BCD)
+          0x0, # Workshop code
           0x7,
           0xe6,
           0x2c,
@@ -227,13 +263,16 @@ with Client(conn, request_timeout=5, config=configs.default_client_config) as cl
 
       client.tester_present()
 
-      def next_counter(counter):
+      def next_counter(counter: int) -> int:
         if(counter == 0xFF):
           return 0
         else:
           return (counter + 1)
 
-      def flash_block(client, block_files, block_number):
+      def flash_block(client: Client, block_files: List[str], block_number: int):
+        logger.info(vin + ": Flashing block: " + str(block_number) + " with " + block_files[block_number])
+        print("Beginning block flashing process for block " + str(block_number) + " : " + int_to_block_name[block_number] + " - with file named " + block_files[block_number] + " ...")
+        
         data = open(block_files[block_number], "rb").read()
 
         print("Erasing block " + str(block_number) + ", routine 0xFF00...")
@@ -274,17 +313,21 @@ with Client(conn, request_timeout=5, config=configs.default_client_config) as cl
         # Checksum
         client.start_routine(0x0202, data=bytes([0x01, block_number, 0, 0x04, 0, 0, 0, 0]))
 
+        logger.info(vin + ": Success flashing block: " + str(block_number) + " with " + block_files[block_number])
+        print("Successfully flashed " + block_files[block_number] + " to block " + str(block_number))
+
       # patch_block takes a block index and subtracts 5 to pick the block to actually patch.
       # for example [1: file1, 2: file2, 3: file3, 4: file4, 9: file4_patch, 5: file5]
-      def patch_block(client, block_files, block_number):
+      def patch_block(client: Client, block_files: List[str], block_number: int):
         data = open(block_files[block_number], "rb").read()
         block_number = block_number - 5
 
-        print("Erasing block " + str(block_number + 1) + ", routine 0xFF00...")
+        print("Erasing next block for PATCH process - erasing block " + str(block_number + 1) + " to patch " + str(block_number) + " routine 0xFF00...")
         # Erase Flash
         client.start_routine(0xFF00, data=bytes([0x1, block_number + 1]))
 
-        print("Requesting download for PATCH block " + str(block_number) + " of length " + str(block_lengths[block_number]) + " ...")
+        logger.info(vin + ": PATCHING block: " + str(block_number) + " with " + block_files[block_number + 5])
+        print("Requesting download to PATCH block " + str(block_number) + " of length " + str(block_lengths[block_number]) + " using file " + block_files[block_number + 5] + " ...")
         # Request Download
         dfi = udsoncan.DataFormatIdentifier(compression=0x0, encryption=0xA)
         memloc = udsoncan.MemoryLocation(block_number, block_lengths[block_number], memorysize_format=32)
@@ -318,6 +361,9 @@ with Client(conn, request_timeout=5, config=configs.default_client_config) as cl
         print("Exiting PATCH transfer...")
         # Exit Transfer
         client.request_transfer_exit()
+        print("PATCH successful.")
+        logger.info(vin + ": PATCHED block: " + str(block_number) + " with " + block_files[block_number + 5])
+
 
       for block in tqdm(block_files, unit="Block"):
         if block <= 5:
@@ -345,8 +391,8 @@ with Client(conn, request_timeout=5, config=configs.default_client_config) as cl
 
       print("Done!")
    except exceptions.NegativeResponseException as e:
-      print('Server refused our request for service %s with code "%s" (0x%02x)' % (e.response.service.get_name(), e.response.code_name, e.response.code))
+      logger.error('Server refused our request for service %s with code "%s" (0x%02x)' % (e.response.service.get_name(), e.response.code_name, e.response.code))
    except exceptions.InvalidResponseException as e:
-      print('Server sent an invalid payload : %s' % e.response.original_payload)
+      logger.error('Server sent an invalid payload : %s' % e.response.original_payload)
    except exceptions.UnexpectedResponseException as e:
-      print('Server sent an invalid payload : %s' % e.response.original_payload)
+      logger.error('Server sent an invalid payload : %s' % e.response.original_payload)
