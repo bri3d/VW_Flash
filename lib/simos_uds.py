c7710746e9402c502fc7663938b2bc82827e6b30
Joe Fischetti <jkfischetti@gmail.com>
2021-02-21 12:23:42 -0500

    simos_uds read_ecu_data cleanup

--- lib/simos_uds.py
+++ lib/simos_uds.py
@@ -371,7 +371,7 @@ def read_ecu_data(interface, callback = None):
         client.config['data_identifiers'][0xF15A] = GenericBytesCodec
 
         if callback:
-          callback(flasher_step = 'SETUP', flasher_status = "Entering extended diagnostic session... ", flasher_progress = 0)
+          callback(flasher_step = 'READING', flasher_status = "Entering extended diagnostic session... ", flasher_progress = 0)
   
         consoleLogger.info("Opening extended diagnostic session...")
         client.change_session(services.DiagnosticSessionControl.Session.extendedDiagnosticSession)
@@ -380,103 +380,19 @@ def read_ecu_data(interface, callback = None):
         vin: str = client.read_data_by_identifier_first(vin_did.address)
        
         if callback:
-          callback(flasher_step = 'SETUP', flasher_status = "Connected to vehicle with VIN: " + vin, flasher_progress = 100)
+          callback(flasher_step = 'READING', flasher_status = "Connected to vehicle with VIN: " + vin, flasher_progress = 100)
 
         consoleLogger.info("Extended diagnostic session connected to vehicle with VIN: " + vin)
-        logger.info(vin + " Connected: Flashing blocks: " + str([block_files[filename]['blocknum'] for filename in block_files]))
-  
+        
         consoleLogger.info("Reading ECU information...")
-        #for i in range(33, 47):
-        #  did = constants.data_records[i]
-        #  response = client.read_data_by_identifier_first(did.address)
-        #  consoleLogger.info(did.description + " : " + response)
-        #  logger.info(vin + " " + did.description + " : " + response)
+        for i in range(33, 47):
+          did = constants.data_records[i]
+          response = client.read_data_by_identifier_first(did.address)
+          consoleLogger.info(did.description + " : " + response)
+          logger.info(vin + " " + did.description + " : " + response)
   
-        # Check Programming Precondition
         if callback:
-          callback(flasher_step = 'SETUP', flasher_status = "Checking programming precondition" , flasher_progress = 100)
-
-        consoleLogger.info("Checking programming precondition, routine 0x0203...")
-        client.start_routine(0x0203)
-  
-        client.tester_present()
-  
-        # Upgrade to Programming Session
-        if callback:
-          callback(flasher_step = 'SETUP', flasher_status = "Upgrading to programming session..." , flasher_progress = 100)
-
-        consoleLogger.info("Upgrading to programming session...")
-        client.change_session(services.DiagnosticSessionControl.Session.programmingSession)
-  
-        # Fix timeouts to work around overly smart library
-        client.session_timing['p2_server_max'] = 30
-        client.config['request_timeout'] = 30
-  
-        client.tester_present()
-
-        if callback:
-          callback(flasher_step = 'SETUP', flasher_status = "Performing Seed/Key authentication..." , flasher_progress = 100)
-  
-        # Perform Seed/Key Security Level 17. This will call volkswagen_security_algo above to perform the Seed/Key auth against the SA2 script.
-        consoleLogger.info("Performing Seed/Key authentication...")
-        client.unlock_security_access(17)
-  
-        client.tester_present()
-
-        if callback:
-          callback(flasher_step = 'SETUP', flasher_status = "Writing Workshop data..." , flasher_progress = 100)
-  
-        consoleLogger.info("Writing flash tool log to LocalIdentifier 0xF15A...")
-        # Write Flash Tool Workshop Log (TODO real/fake date/time, currently hardcoded to 2014/7/17)
-        client.write_data_by_identifier(0xF15A, bytes([
-            0x20, # Year (BCD/HexDecimal since 2000)
-            0x7, # Month (BCD)
-            0x17, # Day (BCD)
-            0x42, # Workshop code
-            0x04,
-            0x20,
-            0x42,
-            0xB1,
-            0x3D
-        ]))
-  
-        client.tester_present()
-
-        for filename in block_files:
-          #pull the relevent filename, blocknum, and binary_data from the dict
-          binary_data = block_files[filename]['binary_data']
-          blocknum = block_files[filename]['blocknum']
-
-          if blocknum <= 5:
-            flash_block(client = client, filename = filename, data = binary_data, block_number = blocknum, vin = vin, callback = callback)
-          if blocknum > 5:
-            patch_block(client, filename, binary_data, blocknum, vin, callback)
-
-        if callback:
-          callback(flasher_step = 'SETUP', flasher_status = "Verifying reprogramming dependencies..." , flasher_progress = 100)
-
-        consoleLogger.info("Verifying programming dependencies, routine 0xFF01...")
-        # Verify Programming Dependencies
-        client.start_routine(Routine.CheckProgrammingDependencies)
-  
-        client.tester_present()
-  
-        # If a periodic task was patched or altered as part of the process, let's give it a few seconds to run
-        time.sleep(5)
-        if callback:
-          callback(flasher_step = 'SETUP', flasher_status = "Finalizing..." , flasher_progress = 100)
-  
-        consoleLogger.info("Rebooting ECU...")
-        # Reboot
-        client.ecu_reset(services.ECUReset.ResetType.hardReset)
-  
-        consoleLogger.info("Sending 0x4 Clear Emissions DTCs over OBD-2")
-        send_obd(bytes([0x4]))
-  
-        client.tester_present()
-
-        if callback:
-          callback(flasher_step = 'SETUP', flasher_status = "DONE!..." , flasher_progress = 100)
+          callback(flasher_step = 'READING', flasher_status = "DONE!..." , flasher_progress = 100)
  
         consoleLogger.info("Done!")
      except exceptions.NegativeResponseException as e:
