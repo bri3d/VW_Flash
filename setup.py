d00a308a342d74aa9307af93d8505f66ac92efdd
bri3d <brian@brianledbetter.com>
2021-07-24 13:27:50 -0600

    add box_codes.csv to exe build

--- setup.py
+++ setup.py
@@ -2,6 +2,6 @@ from distutils.core import setup
 import py2exe
 import lib
 
-data_files = [("lib/lzss", ["lib/lzss/lzss.exe"]), (".", ["logging.conf"])]
+data_files = [("lib/lzss", ["lib/lzss/lzss.exe"]), (".", ["logging.conf"]), ("data", ["box_codes.csv"])]
 
 setup(console=["VW_Flash_GUI.py"], data_files=data_files)
