d7527a12510f2e3d1263d9f9f553ed325414f276
bri3d <brian@brianledbetter.com>
2021-09-26 12:41:37 -0600

    add DSG key to Windows GUI build

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ import lib
 data_files = [
     ("lib/lzss", ["lib/lzss/lzss.exe"]),
     (".", ["logging.conf"]),
-    ("data", ["data/box_codes.csv"]),
+    ("data", ["data/box_codes.csv", "data/mqb_dsg_key.bin"]),
 ]
 
 setup(console=["VW_Flash_GUI.py"], data_files=data_files)
